e4116ed244bc2ec83f0d1273d3e5241de38df807
Ketaki Ghatole <52592007+98ketaki@users.noreply.github.com>
2023-03-04 14:21:46 -0500

    Update align.sh

--- scripts/align.sh
+++ scripts/align.sh
@@ -4,5 +4,5 @@ mkdir output/samfiles
 
 bwa mem -M -t 32 -R "@RG\tID:id\tSM:sample\tLB:lib" reference/ref.fa data/sample1_forward.fastq.gz data/sample1_reverse.fastq.gz  > output/samfiles/sam1.sam
 
-bwa mem -M -t 32 -R "@RG\tID:id\tSM:sample\tLB:lib" reference/ref.fa data/sample2__forward.gz data/sample2_reverse.fastq.gz  > output/samfiles/sam2.sam
+bwa mem -M -t 32 -R "@RG\tID:id\tSM:sample\tLB:lib" reference/ref.fa data/sample2_forward.gz data/sample2_reverse.fastq.gz  > output/samfiles/sam2.sam
 
